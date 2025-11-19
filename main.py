ad7093e8107b853e7facccf24b606e8ce0699ede
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-11-19 15:36:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+#1-misol
+s = "GrokByxAI"
+print(s)
+
+toza = s[4:6]
+print(toza)
+
+#2-misol
+s = "9876543210"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+#3-misol
+s = "MachineLearning"
+print(s)
+
+toza = s[7:-3]
+print(toza)
+
+#4-misol
+s = "PythonJavaJavaScript"
+print(s)
+
+soni = s.count('Java')
+print(f"Java lar soni: {soni}")
+
+res = 'Java' * soni
+print(res)
+
+#5-misol
+s = "abcdefghijk"
+print(s)
+
+toza = s[:5:-5]
+print(toza)
